838abe90668f99450630100a6e00b6a48b379c55
Tokiosman80F <tokiosman96@gmail.com>
2026-05-11 17:40:23 +0600

    feat: display bookmark data at bookmarkgrid

--- src/bookmark/BookMark.jsx
+++ src/bookmark/BookMark.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import BookMarkForm from "./BookMarkForm";
 import BookMarkGrid from "./BookMarkGrid";
 import SearchBookMark from "./SearchBookMark";
 
+const defaultBookMark = {
+  id: crypto.randomUUID(),
+  url: "https://www.pinterest.com/",
+  favColor: "#EE4B2B",
+  category: "Design",
+  user: "tokiosman",
+  pass: "123456",
+};
+
 function BookMark() {
+  const [bookMarks, setBookMarks] = useState([defaultBookMark]);
   function handleAddBookmark(bookmark) {
     console.log(bookmark);
+    setBookMarks(bookMarks);
   }
   return (
     <div>
@@ -12,7 +24,7 @@ function BookMark() {
       <main className="p-8">
         <div className="max-w-7xl mx-auto space-y-10 px-4">
           <SearchBookMark />
-          <BookMarkGrid />
+          <BookMarkGrid bookmarks={bookMarks} />
         </div>
       </main>
     </div>
